27aa397d716b51bf75fe76d650ea6978f52fdf5a
nate.jenson <nate.jenson@protolabs.com>
2016-12-01 17:01:54 -0600

    Complete day one, part two

--- Day 1/day1.py
+++ Day 1/day1.py
@@ -9,6 +9,10 @@ def ChangeHeadingOnTurn(startHeading, turn):
     else:
         return (startHeading - 1) % 4
 
+def DistanceBetween(startX, startY, endX, endY):
+    return abs(endX - startX) + abs(endY - startY)
+    
+# Part 1
 def MoveToEnd(startX, startY, directions):
     heading = 0 # 0,1,2,3 -> N, E, S, W
     x,y = startX, startY
@@ -30,11 +34,41 @@ def Move(startX, startY, heading, distance):
         x -= distance
     return (x,y)
 
-def DistanceBetween(startX, startY, endX, endY):
-    return abs(endX - startX) + abs(endY - startY)
+# Part 2
+def GetPointsOnPath(startX, startY, heading, distance):
+    if (heading == 0):
+        return [(startX, step) for step in range(startY+1,startY+distance+1)]
+    elif (heading == 1):
+        return [(step, startY) for step in range(startX+1,startX+distance+1)]
+    elif (heading == 2):
+        return [(startX, step) for step in range(startY-1,startY-distance-1,-1)]
+    elif (heading == 3):
+        return [(step, startY) for step in range(startX-1,startX-distance-1,-1)]
+
+def MoveToFirstDuplicate(startX, startY, direction):
+    visited = set()
+    visited.add((startX,startY))
+    heading = 0
+    x,y = startX, startY
+    for direction in directions:
+        turn, distance = direction[0], int(direction[1:])
+        heading = ChangeHeadingOnTurn(heading, turn)
+        line = GetPointsOnPath(x,y,heading,distance)
+        for point in line:
+            if point in visited:
+                return point
+            visited.add(point)
+        # set x,y to the last point on the line we just walked.
+        x,y = line[-1][0], line[-1][1]
+    return None
+
+
 
 directions = GetDirections()
 
-(x,y) = MoveToEnd(0,0,directions)
-print("Part 1 end point: " + str(x) + "," + str(y))
-print("Part 1 distance:", DistanceBetween(0,0,x,y))
+x,y = MoveToEnd(0,0,directions)
+print("Part 1 - End point:", str((x,y)))
+print("Part 1 - Distance:", DistanceBetween(0,0,x,y))
+x2,y2 = MoveToFirstDuplicate(0,0,directions)
+print("Part 2 - First Duplicate:", str((x2,y2)))
+print("Part 2 - Distance:", DistanceBetween(0,0,x2,y2))
