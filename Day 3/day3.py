1f37e8b3a9785ebf3dbbb0da9c6b0e79468cb63f
Nate Jenson <nate.m.jenson@gmail.com>
2016-12-05 19:31:17 -0600

    Complete day 3, part two

--- Day 3/day3.py
+++ Day 3/day3.py
@@ -7,14 +7,24 @@ def isTriangleBad(sides):
     sides = sorted(sides)
     return sides[0] + sides[1] <= sides[2]
 
-def getNumberGoodTriangles(triangles):
+def numGoodTriangles(triangles):
     count = 0
     for triangle in triangles:
         if(not isTriangleBad(triangle)):
             count += 1
     return count
 
+def numGoodTrianglesVertical(triangles):
+    count = 0
+    for column in range(len(triangles[0])):
+        for row in range(0,len(triangles),3):
+            if(not isTriangleBad([triangles[row][column],
+                    triangles[row+1][column],
+                    triangles[row+2][column]])):
+                count += 1
+    return count
 
 
 triangles = getTriangles()
-print('Part 1 - Number bad triangles:', getNumberGoodTriangles(triangles))
+print('Part 1 - Number good triangles:', numGoodTriangles(triangles))
+print('Part 2 - Number good triangles vertically:', numGoodTrianglesVertical(triangles))
