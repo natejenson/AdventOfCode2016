982c2bee7cbd2bcece11377cd85080dac1142498
nate.jenson <nate.jenson@protolabs.com>
2016-12-15 14:59:02 -0600

    Complete day 4, part two

--- Day 4/day4.py
+++ Day 4/day4.py
@@ -7,6 +7,15 @@ class Room:
         self.encryptedName = inputStr[:lastDashIdx]
         self.sectorId = int(inputStr[lastDashIdx+1:firstBracketIdx])
         self.checksum = inputStr[firstBracketIdx+1:-1]
+
+    def decryptName(self):
+        realName = ""
+        for c in self.encryptedName:
+            if (c == '-'):
+                realName += " "
+            else:
+                realName += chr(ord('a') + ((ord(c)-ord('a')+self.sectorId)%26))
+        return realName
         
     def isReal(self):
         occurences = defaultdict(int)
@@ -28,6 +37,9 @@ def GetRooms():
 rooms = GetRooms()
 sumRealRooms = sum(r.sectorId for r in rooms if r.isReal())
 print('Part 1 - Sum of sector IDs of real rooms:', sumRealRooms)
+print('Part 2 - North Pole Objects room:', [r.sectorId for r in rooms if "northpole" in r.decryptName()][0])
+
+
 
 
     
