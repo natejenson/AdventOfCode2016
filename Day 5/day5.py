43ae18bcdedf542138df280f38235f52795f2d42
nate.jenson <nate.jenson@protolabs.com>
2016-12-16 12:54:09 -0600

    Complete day 5, part two

--- Day 5/day5.py
+++ Day 5/day5.py
@@ -1,5 +1,6 @@
 import hashlib
-
+import time
+    
 def GetPasswordForDoor(doorId):
     password = ""
     i = 0
@@ -11,7 +12,31 @@ def GetPasswordForDoor(doorId):
         i = i + 1
     return password
 
+def GetPasswordForDoorTwo(doorId):
+    password = [None for i in range(8)]
+    foundChars = 0
+    i = 0
+    while (foundChars < len(password)):
+        m = hashlib.md5((doorId + str(i)).encode('utf-8'))
+        hashed = m.hexdigest()
+        if(hashed[:5] == "00000"):
+            position = ord(hashed[5]) - ord('0')
+            newChar = hashed[6]
+            if (position < len(password) and password[position] == None):
+                password[position] = newChar
+                foundChars += 1
+        i = i + 1
+    return password
+
 
 doorId = "uqwqemis"
+
+start_time = time.clock()
 password = GetPasswordForDoor(doorId)
 print('Part 1 - Door Password:', password)
+print('Part 1 - %.3f seconds' % (time.clock() - start_time))
+
+start_time = time.clock()
+password = GetPasswordForDoorTwo(doorId)
+print('Part 2 - Door Password:', password)
+print('Part 2 - %.3f seconds' % (time.clock() - start_time))
